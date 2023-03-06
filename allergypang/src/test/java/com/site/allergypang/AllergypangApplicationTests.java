4e8024bb8b97a5cddc2ee78cfdd7f9d8453964ac
sb121300 <soobin851@kookmin.ac.kr>
2023-03-06 17:16:41 +0900

    [Feat] main page 생성

---
+++ allergypang/src/test/java/com/site/allergypang/AllergypangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.site.allergypang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllergypangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
