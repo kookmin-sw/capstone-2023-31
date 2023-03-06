49397c5cf56e12e6edc9892881b541544cdd8c94
sb121300 <soobin851@kookmin.ac.kr>
2023-03-06 21:11:22 +0900

    [Feat] react-spring boot 연동

--- backend/src/main/java/com/site/backend/BackendController.java
+++ backend/src/main/java/com/site/backend/BackendController.java
@@ -1,7 +1,11 @@
 package com.site.backend;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class BackendController {
@@ -9,5 +13,15 @@ public class BackendController {
 	@GetMapping("/")
 	public String main() {
 		return "main";
+	}
+	
+	
+	@RestController
+	public class HelloController {
+
+	    @GetMapping("/api/hello")
+	    public List<String> Hello(){
+	        return Arrays.asList("서버서버", "뷰뷰");
+	    }
 	}
 }
