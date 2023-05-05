c1be6087faa1f13a1d240c56df05c8d0123b16bc
sb121300 <soobin851@kookmin.ac.kr>
2023-05-05 17:52:31 +0900

    [Add] 이미지 크롭

--- data/src/crop_image.py
+++ data/src/crop_image.py
@@ -3,7 +3,7 @@ import os
 
 
 # 이미지 경로 설정
-base_dir="/Users/gimsubin/Desktop/2023/2023-1/real_capstone/capstone-2023-31/data/dataset/crop_dataset/test"
+base_dir="/Users/gimsubin/Desktop/2023/2023-1/real_capstone/capstone-2023-31/data/dataset/crop_dataset/modify"
 dirlist=["heart","oblong","oval","round","square"]
 
 # 사진에서 얼굴 인식을 위해 cascade classifier 로드
@@ -15,13 +15,13 @@ for diry in dirlist:
         # 이미지 읽기
         img = cv2.imread(img_path)
 
-        faces = face_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=5)
+        faces = face_cascade.detectMultiScale(img, scaleFactor=1.05, minNeighbors=6, minSize=(50, 50))
 
 
         
         for (x, y, w, h) in faces:
-            
-            face = img[y:y+h, x:x+w]
+            new_h=int((1.2)*h)
+            face = img[y:y+new_h, x:x+w]
             # 크롭된 이미지 저장
             cv2.imwrite(img_path,face)
     print(diry,"crop 완")
