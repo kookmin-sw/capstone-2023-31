5b6cced247a1c44335e19351a550a858a158c0d4
sb121300 <soobin851@kookmin.ac.kr>
2023-05-05 13:47:05 +0900

    [Add] 이미지 증강

--- data/src/augmentation.py
+++ data/src/augmentation.py
@@ -1,16 +1,25 @@
 import Augmentor
+import os
 
-## 증강 시킬 이미지 폴더 경로
-img = Augmentor.Pipeline("D:/이미지 증강")
+root_dir = os.path.join(os.getcwd(),"faceshape_master/published_dataset/train")
 
-## 좌우 반전
-img.flip_left_right(probability=1.0) 
 
-## 상하 반전
-img.flip_top_bottom(probability=1.0)
+# loop over each class
+dirlist=["heart","oblong","oval","round","square"]
+for face_dir in dirlist:
+	base_dir=os.path.join(root_dir,face_dir)
+	for file in os.listdir(base_dir):
+		# 이미지 증강할 데이터셋 경로 설정
+		p = Augmentor.Pipeline(base_dir)
 
-## 왜곡
-img.random_distortion(probability=1, grid_width=10, grid_height=10, magnitude=8)
+		# 다양한 증강 기법 적용
+		p.rotate(probability=0.5, max_left_rotation=10, max_right_rotation=10)
+		p.zoom(probability=0.5, min_factor=1.1, max_factor=1.5)
+		p.flip_left_right(probability=0.5)
+		p.flip_top_bottom(probability=0.5)
+		p.skew(probability=0.5, magnitude=0.3)
+		p.random_distortion(probability=0.5, grid_width=4, grid_height=4, magnitude=8)
 
-## 증강 이미지 수
-img.sample(10)
+		# 증강된 이미지 저장 폴더 경로 설정
+		#save_dir=os.path.join(root_dir,"save/"+face_dir)
+		p.sample(1000)    
