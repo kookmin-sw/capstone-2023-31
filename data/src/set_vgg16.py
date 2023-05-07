d8d628e10db5d74e9d90f81e3d2860284e991a8a
sb121300 <soobin851@kookmin.ac.kr>
2023-05-08 07:27:56 +0900

    [Add] vgg 모델링 파일

--- data/src/set_vgg16.py
+++ data/src/set_vgg16.py
@@ -2,20 +2,25 @@ from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras.applications.vgg16 import preprocess_input
 from tensorflow.keras.models import Model
 import numpy as np
+import os
+
+#기본 설정
+dataset_path="/Users/gimsubin/Desktop/2023/2023-1/real_capstone/capstone-2023-31/data/dataset/dataset/train/reduce_data"
 
 # VGG16 모델 불러오기
 base_model = VGG16(weights='imagenet')
 model = Model(inputs=base_model.input, outputs=base_model.get_layer('fc1').output)
 
+
 # 얼굴형 클래스
 face_types = ["heart", "oval", "round", "square", "oblong"]
-
 # 각 얼굴형별 npz 파일들을 읽어 특징 벡터 추출
 for face_type in face_types:
     combined_features = []
-    
-    for i in range(44):  # 파일 번호 범위에 맞게 수정
-        filename = os.path.join(dataset_path, "data", "{}_preprocessed_data_{}.npz".format(face_type, i))
+    file_path=os.path.join(dataset_path,face_type)
+    for file in os.listdir(file_path):
+        if "DS" in file: continue
+        filename=os.path.join(file_path,file)
         data = np.load(filename)
         images = data['images']
 
