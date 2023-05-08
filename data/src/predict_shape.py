bcc6c05f69553f335f7b0fb774f694cbcbf026c8
sb121300 <soobin851@kookmin.ac.kr>
2023-05-08 19:07:26 +0900

    [Add] face 모델을 불러와 변환하는 코드

--- data/src/predict_shape.py
+++ data/src/predict_shape.py
@@ -4,10 +4,12 @@ import tensorflow
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras.applications.vgg16 import preprocess_input
-from tensorflow.keras.models import Model
+from tensorflow.keras.models import Model, load_model
 import os
-
 from sklearn.neighbors import KNeighborsClassifier
+
+model = load_model("face_shape_model.h5")
+
 
 base_path="/Users/gimsubin/Desktop/2023/2023-1/real_capstone/capstone-2023-31/data/dataset/dataset/train_vector"
 
@@ -32,14 +34,8 @@ labels = np.concatenate((heart_labels, oblong_labels, oval_labels, round_labels,
 # Train a k-nearest neighbors classifier
 knn = KNeighborsClassifier(n_neighbors=3)
 knn.fit(data, labels)
-
 # 카테고리(얼굴형) 리스트
 face_shapes = ['heart', 'oblong', 'oval', 'round', 'square']
-
-# VGG16 모델 불러오기
-base_model = VGG16(weights='imagenet')
-model = Model(inputs=base_model.input, outputs=base_model.get_layer('block5_pool').output)
-
 
 # Function to load an image
 def load_image(image_path):
