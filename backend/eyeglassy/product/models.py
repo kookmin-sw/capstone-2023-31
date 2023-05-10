ddff9f4e986ddeb1a8a7f476c93b59af28c4bb3c
sb121300 <soobin851@kookmin.ac.kr>
2023-05-11 05:33:23 +0900

    [Add] user 모델 생성

---
+++ backend/eyeglassy/product/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Glasses(models.Model):
+    # 안경 모델의 필드 정의
+    pass
--- backend/eyeglassy/user/models.py
+++ backend/eyeglassy/user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from product import models
 
-# Create your models here.
+class User(models.Model):
+    nickname = models.CharField(max_length=30)
+    password = models.CharField(max_length=128)
+    face_shape = models.ImageField(upload_to='user_images/')
+    glasses = models.ManyToManyField('Glasses', related_name='users')
+
+
