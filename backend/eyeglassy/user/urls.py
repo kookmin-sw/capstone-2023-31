80d3118ac9b3dcdc726e3cf43e81731550afb688
sb121300 <soobin851@kookmin.ac.kr>
2023-05-12 17:42:48 +0900

    [ADD] 회원가입 백엔드-프론트엔드 구현

--- backend/eyeglassy/user/urls.py
+++ backend/eyeglassy/user/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from analyze import views
+from user import views
 from django.views.decorators.csrf import csrf_exempt
 app_name = "user"
 
 urlpatterns = [
-    path("", views.main, name="main"),#삭제 후, 배포
-    path('signup/', views.analyze_face, name='analyze_face'),
-    path('get-csrf-token/', csrf_exempt(views.get_csrf_token),
-         name='get_csrf_token'),
+    #path("", views.main, name="main"),#삭제 후, 배포
+    #path('signup', views.signup, name='signup'),  # 회원가입 페이지를 띄우는 url
+    path('register/', views.register, name='register'), #회원가입을 처리하는 url
+    
+    path('get-csrf-token/', csrf_exempt(views.get_csrf_token),name='get_csrf_token'),
 
 ]
--- backend/eyeglassy/user/views.py
+++ backend/eyeglassy/user/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.response import Response
+from rest_framework import status
+
+from django.middleware.csrf import get_token
+from rest_framework.decorators import api_view
+from django.contrib.auth.hashers import make_password
 
 from .models import User
 
@@ -7,11 +14,27 @@ from .models import User
 @api_view(['GET'])
 def get_csrf_token(request):
     # 클라이언트에게 CSRF 토큰을 반환
-    return JsonResponse({'csrfToken': get_token(request)})
+    return Response({'csrfToken': get_token(request)})
 
 
-def signup(request):
-    if request.method == 'POST':
-        nickname = request.POST['nickname']
-        password = request.POST['password']
-        
+@api_view(['POST'])
+def register(request):
+    try:
+        # image_file = request.FILES['image']
+        email = request.data.get("email")
+        nickname = request.data.get('nickname')
+        password = request.data.get('password')
+
+        # 비밀번호를 해시하여 저장
+        hashed_password = make_password(password)
+        print("성공")
+    except KeyError:
+        print("실패")
+        return Response({'message': '이미지 파일이 필요합니다.'}, status=status.HTTP_400_BAD_REQUEST)
+
+    # 사용자 생성
+    user = User.objects.create(
+        email=email, nickname=nickname, password=hashed_password)
+
+    # 사용자 정보 반환
+    return JsonResponse({'success': True, 'message': '회원가입이 완료되었습니다.'})
