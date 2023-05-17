9fccb45e30e95633205afec99d91096f5db4a79b
se0983 <se098300@gmail.com>
2023-05-18 03:49:39 +0900

    [Modify] 얼굴인식 opencv 모드 시 피팅 안경 돌아가는 문제 해결

--- backend/eyeglassy/fitting/views.py
+++ backend/eyeglassy/fitting/views.py
@@ -87,8 +87,18 @@ def cv_prac_fitting(glasses_path, image_file):
     eyes = eye_cascade.detectMultiScale(face_img_gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
     # x y w h
-    left_eye = ((int(eyes[0][0]) + (int(eyes[0][2])//2)), (int(eyes[0][1]) + (int(eyes[0][3])//2)))
-    right_eye = ((int(eyes[1][0]) + (int(eyes[1][2])//2)), (int(eyes[1][1]) + (int(eyes[1][3])//2)))
+    # left_eye = ((int(eyes[0][0]) + (int(eyes[0][2])//2)), (int(eyes[0][1]) + (int(eyes[0][3])//2)))
+    # right_eye = ((int(eyes[1][0]) + (int(eyes[1][2])//2)), (int(eyes[1][1]) + (int(eyes[1][3])//2)))
+
+    left_eye_x = int(eyes[0][0]) + (int(eyes[0][2]) // 2)
+    right_eye_x = int(eyes[1][0]) + (int(eyes[1][2]) // 2)
+
+    if left_eye_x < right_eye_x:
+        left_eye = (left_eye_x, int(eyes[0][1]) + (int(eyes[0][3]) // 2))
+        right_eye = (right_eye_x, int(eyes[1][1]) + (int(eyes[1][3]) // 2))
+    else:
+        left_eye = (right_eye_x, int(eyes[1][1]) + (int(eyes[1][3]) // 2))
+        right_eye = (left_eye_x, int(eyes[0][1]) + (int(eyes[0][3]) // 2))
 
     # cv2.circle(face_img, left_eye, 3, (0, 255, 0), -1) # green
     # cv2.circle(face_img, right_eye, 3, (0, 255, 0), -1)
@@ -164,9 +174,9 @@ def fitting_face(request, id):
 
 
     ### 4) 이미지 위에 안경 이미지 붙여서 반환
-    ### CHOICE : dlib or cv
-    fitted_face = dlib_prac_fitting(glasses_path, image_path) # 누끼딴 안경 이미지 경로, 얼굴 이미지 경로
-    # fitted_face = cv_prac_fitting(glasses_path, image_path)
+    ### 모드 선택 CHOICE : dlib or cv
+    # fitted_face = dlib_prac_fitting(glasses_path, image_path) # 누끼딴 안경 이미지 경로, 얼굴 이미지 경로
+    fitted_face = cv_prac_fitting(glasses_path, image_path)
 
 
     # response_data = {'result': 'success', 'message': '이미지 처리 완료'}
